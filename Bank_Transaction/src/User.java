742c7b1c235c77e6a04e60057e245823aeaa0e77
RathanakSeng <40135662+RathanakSeng@users.noreply.github.com>
2018-12-07 12:24:03 +0700

    Update_)7/Dec/2018

---
+++ Bank_Transaction/src/User.java
@@ -0,0 +1,13 @@
+public class User
+{
+    private Integer id;
+    private String name;
+    private Double cash;
+
+    public User(Integer id, String name, Double cash)
+    {
+        this.id = id;
+        this.name = name;
+        this.cash = cash;
+    }
+}
