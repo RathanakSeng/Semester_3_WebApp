f5d60931e9575862edfd000f5c056ecd3b4b2c21
RathanakSeng <40135662+RathanakSeng@users.noreply.github.com>
2018-12-16 13:36:52 +0700

    JoinRowSetExample_16/Dec/2018

---
+++ MyJoinRowSet/src/JoinRowSetExample.java
@@ -0,0 +1,42 @@
+import com.sun.rowset.CachedRowSetImpl;
+import com.sun.rowset.JoinRowSetImpl;
+
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.JoinRowSet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+/**
+ * @author Rathanak
+ */
+public class JoinRowSetExample
+{
+    public static void main(String[] args) throws Exception{
+
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_database", "root", "n@k123seng");
+
+        JoinRowSet jrs = new JoinRowSetImpl();
+
+        ResultSet rs_1 = con.createStatement().executeQuery("select dept_name from department");
+        CachedRowSet crs_1 = new CachedRowSetImpl();
+        crs_1.populate(rs_1);
+        crs_1.setMatchColumn(1);
+        jrs.addRowSet(crs_1);
+
+        ResultSet rs_2 = con.createStatement().executeQuery("select dept_name from student");
+        CachedRowSet crs_2 = new CachedRowSetImpl();
+        crs_2.populate(rs_2);
+        crs_2.setMatchColumn(1);
+        jrs.addRowSet(crs_2);
+
+        jrs.first();
+        do {
+            System.out.println(jrs.getString("dept_name"));
+        } while (jrs.next());
+    }
+
+    /**
+     * A simple rowSet technology is used to perform join operator in SQL
+     */
+}
